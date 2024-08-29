00da650e724438b3a78660afe0f5fa7d1884cb81
Siqius <Real.FBAB@gmail.com>
2024-08-29 12:00:46 +0200

    working addon, need to fix: windows api click

--- mouseController.cpp
+++ mouseController.cpp
@@ -1,25 +1,55 @@
-#include <windows.h>
-#include <node_api.h>
 #include <node.h>
+#include <windows.h>
+#include <winuser.h>
 
-namespace demo {
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::String;
+using v8::Local;
+using v8::Number;
+using v8::Exception;
+using v8::Value;
 
-    using v8::FunctionCallbackInfo;
-    using v8::Isolate;
-    using v8::Local;
-    using v8::Object;
-    using v8::String;
-    using v8::Value;
+void Click(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  bool pos = false;
+  INPUT inputs[4] = {};
+  ZeroMemory(inputs, sizeof(inputs));
 
-    void Method(const FunctionCallbackInfo<Value>& args) {
-        Isolate* isolate = args.GetIsolate();
-        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world").ToLocalChecked());
+  inputs[0].type = INPUT_MOUSE;
+  inputs[0].ki.wVk = VK_LEFT;
+  inputs[0].ki.dwFlags = MOUSEEVENTF_LEFTDOWN;
+
+  UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+
+  args.GetReturnValue().Set(1);
+
+  /*
+  if (args.Length() > 3 || args.Length() < 1) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate,
+                            "Wrong number of arguments").ToLocalChecked()));
+    return;
+  }
+  if(args.Length() == 3) { 
+    if (!args[1]->IsNumber() || !args[2]->IsNumber()) {
+      isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments").ToLocalChecked()));
+      return;
     }
+    pos = true;
+  }
 
-    void Initialize(Local<Object> exports) {
-        NODE_SET_METHOD(exports, "hello", Method);
-    }
+  double value =
+      args[0].As<Number>()->Value() + args[1].As<Number>()->Value();
+  Local<Number> num = Number::New(isolate, value);
 
-    NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+  args.GetReturnValue().Set(num);
+  */
+}
 
-}  // namespace demo
+
+void Initialize(v8::Local<v8::Object> exports) {
+    NODE_SET_METHOD(exports, "click", Click);
+}
+
+NODE_MODULE(addon, Initialize)
